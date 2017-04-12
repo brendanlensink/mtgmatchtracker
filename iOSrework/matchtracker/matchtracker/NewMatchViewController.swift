74ad4ecffeae90af10827f94024f453b6e6f2ebe
Brendan Lensink <blensink192@gmail.com>
2017-04-12 16:56:50 -0700

    added blank new match controller

---
+++ iOSrework/matchtracker/matchtracker/NewMatchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewMatchViewController.swift
+//  matchtracker
+//
+//  Created by Brendan Lensink on 2017-04-12.
+//  Copyright © 2017 blensink. All rights reserved.
+//
+
+import UIKit
+
+class NewMatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
