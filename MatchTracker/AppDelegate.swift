3dc8da97b51df30a5ec52e1adbdba74c8d6c36fc
Brendan Lensink <blensink192@gmail.com>
2017-05-10 20:28:23 -0700

    added a bunch of image assets and the tab/nav bar controllers

--- MatchTracker/AppDelegate.swift
+++ MatchTracker/AppDelegate.swift
@@ -13,11 +13,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = MatchViewController()
+        
+        let newMatch = UINavigationController(rootViewController: MatchViewController())
+        newMatch.tabBarItem = UITabBarItem(
+            title: "Log A Match",
+            image: UIImage(named: "tab_new"),
+            tag:  1
+        )
+        
+        let matchHistory = UINavigationController(rootViewController: MatchViewController())
+        matchHistory.tabBarItem = UITabBarItem(
+            title: "View Past Matches",
+            image: UIImage(named: "tab_history"),
+            tag: 2
+        )
+        
+        let tabViews = [
+            newMatch,
+            matchHistory
+        ]
+        let tabBar = UITabBarController()
+        tabBar.tabBar.tintColor = UIColor.white
+        tabBar.viewControllers = tabViews
+        
+        self.window?.rootViewController = tabBar
         self.window?.makeKeyAndVisible()
         
         return true
